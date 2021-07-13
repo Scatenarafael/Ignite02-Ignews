f2b49beaa006c18a72d82bbd82569d0c6b188841
Scatenarafael <rafascatena@gmail.com>
2021-07-13 17:46:24 -0300

    implementando testes com jest

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,31 @@
+import { render } from "@testing-library/react";
+import { ActiveLink } from ".";
+
+describe("ActiveLink component", () => {
+  jest.mock('next/router', () => {
+    return {
+      useRouter() {
+        return {
+          asPath: "/",
+        };
+      },
+    };
+  });
+
+  it("renders correctly", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+  it("adds active class if the link is currently active", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+    expect(getByText("Home")).toHaveClass("active");
+  });
+});
--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -22,12 +22,10 @@ export default function PostPreview({ post }: PostPreviewProps) {
   const [session] = useSession();
   const router = useRouter();
 
-
   useEffect(() => {
     if (session?.activeSubscription) {
       router.push(`/posts/${post.slug}`);
     }
-
   }, [session])
 
   return (
@@ -62,7 +60,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [],
+    paths: [ ],
     fallback: 'blocking'
   }
 }
